7a2a4a5b67525d993903210903b3fe3d8c15433d
Rayalisson Pastor <99136117+RayalissonP@users.noreply.github.com>
2022-09-18 01:13:14 -0300

    Add botão para voltar ao top

---
+++ assets/js/script_help.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btn-top")
+
+btn.addEventListener("click", function(){
+   window.scrollTo(0,0)
+})
+
+document.addEventListener("scroll", ocultarBotaoTopo)
+
+function ocultarBotaoTopo() {
+   if(window.scrollY > 200){
+      btn.style.display = "flex"
+   } else {
+      btn.style.display = "none"
+   }
+}
+
+ocultarBotaoTopo()
